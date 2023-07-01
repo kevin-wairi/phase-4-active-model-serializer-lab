15c73d42558c38d0776800c34c9848e1175462af
kevin-wairi <kevinwairi@gmail.com>
2023-07-01 11:25:54 +0300

    assignment completed

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,9 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :title,:short_content,:tags,:content
+  has_many :tags
+  belongs_to :author
+
+  def short_content
+    "#{self.object.content[0..39]...}"
+  end
+end
